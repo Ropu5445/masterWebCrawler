46eff46d61cf418bc01075293a2d76804ec485ac
AaroInginmaa <aaro.inginmaa@gmail.com>
2024-02-14 16:50:21 +0200

    Lisätty kaikki filut

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const table = document.getElementById("data-table")
+var jsonData
+
+async function fetchData(file) {
+    try {
+        const response = await fetch("/src/data/" + file);
+        jsonData = await response.json();
+        await handleData(file)
+    } catch(error) {
+        console.log("Error: ", error)
+    }
+}
+
+function handleData() {
+    removeAllChildNodes(table)
+
+    let cols = Object.keys(jsonData[0]);
+
+    let thead = document.createElement("thead");
+    let tr = document.createElement("tr");
+    let tbody = document.createElement("tbody");
+
+    cols.forEach((item) => {
+        let th = document.createElement("th");
+        let modItem = item[0].toUpperCase() + item.slice(1)
+        th.innerText = modItem; // Set the column name as the text of the header cell
+        tr.appendChild(th); // Append the header cell to the header row
+     });
+
+    thead.appendChild(tr); // Append the header row to the header
+    table.append(thead) // Append the header to the table
+    table.append(tbody)
+
+    jsonData.forEach((item) => {
+        let tr = document.createElement("tr");
+        
+        // Get the values of the current object in the JSON data
+        let vals = Object.values(item);
+        
+        // Loop through the values and create table cells
+        vals.forEach((elem) => {
+           let td = document.createElement("td");
+           td.innerText = elem; // Set the value as the text of the table cell
+           tr.appendChild(td); // Append the table cell to the table row
+        });
+        tbody.appendChild(tr); // Append the table row to the table
+    });
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+fetchData("test.json")
