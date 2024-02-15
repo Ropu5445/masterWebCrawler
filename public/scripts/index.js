a9411f15eaa4ebb7be960e7c6ed19e3ef8d65288
AaroInginmaa <aaro.inginmaa@gmail.com>
2024-02-15 10:58:45 +0200

    Nyt näyttää miak tabi on aktiivinen

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,24 +13,24 @@ async function fetchData(file) {
 
 function handleData() {
     removeAllChildNodes(table)
-
+    
     let cols = Object.keys(jsonData[0]);
-
+    
     let thead = document.createElement("thead");
     let tr = document.createElement("tr");
     let tbody = document.createElement("tbody");
-
+    
     cols.forEach((item) => {
         let th = document.createElement("th");
         let modItem = item[0].toUpperCase() + item.slice(1)
         th.innerText = modItem; // Set the column name as the text of the header cell
         tr.appendChild(th); // Append the header cell to the header row
-     });
-
+    });
+    
     thead.appendChild(tr); // Append the header row to the header
     table.appendChild(thead) // Append the header to the table
     table.appendChild(tbody)
-
+    
     jsonData.forEach((item) => {
         let tr = document.createElement("tr");
         
@@ -39,9 +39,9 @@ function handleData() {
         
         // Loop through the values and create table cells
         vals.forEach((elem) => {
-           let td = document.createElement("td");
-           td.innerText = elem; // Set the value as the text of the table cell
-           tr.appendChild(td); // Append the table cell to the table row
+            let td = document.createElement("td");
+            td.innerText = elem; // Set the value as the text of the table cell
+            tr.appendChild(td); // Append the table cell to the table row
         });
         tbody.appendChild(tr); // Append the table row to the table
     });
@@ -53,6 +53,15 @@ function removeAllChildNodes(parent) {
             parent.removeChild(parent.firstChild);
         }
     }
+}
+
+function addActiveOnClick(elem) {
+    var a = document.getElementsByTagName('a');
+    for (i = 0; i < a.length; i++) {
+        a[i].parentNode.classList.remove('is-active')
+    }
+    // add 'active' classs to the element that was clicked
+    elem.parentNode.classList.add('is-active');
 }
 
 fetchData("test.json")
