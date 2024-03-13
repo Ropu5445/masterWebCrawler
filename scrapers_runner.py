d9b4c21af674d226d77c7a72decb23dd08fb7217
AaroInginmaa <aaro.inginmaa@gmail.com>
2024-03-13 08:40:01 +0200

    Korjattu se, että scriptit käynnistyisi uudestaan loputtomasti

--- scrapers_runner.py
+++ scrapers_runner.py
@@ -2,9 +2,11 @@ import glob, os
 
 os.chdir("./")
 
+ignoredScripts = ['scrapers_runner.py', 'flight.py', 'scrape.py']
+ranScripts = []
+
 for file in glob.glob("*.py"):
-    if file in ['flight.py', 'scrape.py']:
-        pass
-    else:
-        print(f"Running script {file}")
+    if file not in ignoredScripts and file not in ranScripts:
+        print(f"\nRunning script {file}")
+        ranScripts.append(file)
         os.system(f"python {file}");
